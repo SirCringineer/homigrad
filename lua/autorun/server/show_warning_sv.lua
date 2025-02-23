fda2bdd4ee9540eddd096cb44caf4d77594e20fe
Jonny_Bro (Nikita) <48434875+JonnyBro@users.noreply.github.com>
2025-02-23 14:24:54 +0500

    refactor: cleanup

--- lua/autorun/server/show_warning_sv.lua
+++ lua/autorun/server/show_warning_sv.lua
@@ -1,8 +1,7 @@
--- Create a unique identifier for checking if the player has seen the window.
 util.AddNetworkString("ShowWelcomeWindow")
 
 -- Check if the player has already joined the server before
-hook.Add("PlayerInitialSpawn", "ShowWindowOncePerPlayer", function(ply)
+hook.Add("PlayerInitialSpawn", "hg_WelcomeWindow", function(ply)
 	if ply:IsBot() then return end
 
 	local window_file = "homigrad/window_showed.txt"
@@ -12,9 +11,9 @@ hook.Add("PlayerInitialSpawn", "ShowWindowOncePerPlayer", function(ply)
 	local tbl = util.JSONToTable(window_file_content)
 
 	if not tbl[ply:SteamID64()] then
-			-- Send net message to client to show window
 		tbl[ply:SteamID64()] = true
 
+		-- Send net message to client to show window
 		net.Start("ShowWelcomeWindow")
 		net.Send(ply)
 
