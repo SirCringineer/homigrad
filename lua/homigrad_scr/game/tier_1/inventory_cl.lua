bfc857efdf9ccf2d8bcc3605bd23d62f44177398
Niikbb <asd0l@yandex.ru>
2025-02-24 07:19:01 +0300

    Inventory: Forget inventory after round change. (#6)
    * Forget my inventory after round change please.
    
    * Add back one commentary.
    
    * Add one commentary back
    
    * Add "hg_LootAlive" as option in homigrad.txt
    
    * update homigrad.txt
    
    * update homigrad.txt

--- lua/homigrad_scr/game/tier_1/inventory_cl.lua
+++ lua/homigrad_scr/game/tier_1/inventory_cl.lua
@@ -69,8 +69,6 @@ net.Receive("inventory", function()
 
 	local items_ammo = net.ReadTable()
 
-	-- if #items == 0 and #items_ammo == 0 then return end -- wtf
-
 	items.weapon_hands = nil
 
 	panel = vgui.Create("DFrame")
@@ -96,7 +94,6 @@ net.Receive("inventory", function()
 			net.WriteEntity(lootEnt)
 		net.SendToServer()
 	end
-
 	-- Should do the job
 	local lootingTime = math.Clamp(GetConVar("hg_SearchTime"):GetInt(), 0, 10)
 	local targetID = IsValid(lootEnt) and lootEnt:SteamID64()
@@ -215,7 +212,6 @@ net.Receive("inventory", function()
 	end)
 
 	-- "Forget" target's inventory after 1 min
-	timer.Simple(60, function()
-		hg_searched[targetID] = nil
-	end)
+	timer.Simple(60, function() hg_searched[targetID] = nil end)
+	hook.Add("PostCleanupMap", "Forget_Anyones_Inventory", function() hg_searched[targetID] = nil end)
 end)
